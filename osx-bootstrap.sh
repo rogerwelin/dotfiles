20be1033069e00edf625d0c90b82cdc0e0f72df5
Roger Welin <roger.welin@icloud.com>
2023-03-19 15:36:40 +0100

    Create osx-bootstrap.sh

---
+++ osx-bootstrap.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew install ripgrep fd jq htop iamlive ipcalc kind lolcat neovim shellcheck sqlite wget sqlite terraform terraformer tree git figlet bat 
+
+brew tap homebrew/cask-fonts
+brew install font-hack-nerd-font
